a626f3160c926eeb704f7ed8c955b0aca4b3e9d5
Pluto <thyagohyung@gmail.com>
2021-07-09 23:47:35 -0300

    Terminando Template de triagem/login de usuário.

--- iniciante/intro-component-with-signup-form-master/script.js
+++ iniciante/intro-component-with-signup-form-master/script.js
@@ -1,5 +1,16 @@
-const form = {
-   validacaoEmail() {
+const Form = {
+   validation() {
+      if (!Form.validationEmail()) {
+         console.log("email inválido");
+      } else {
+         console.log("email.válido");
+      }
+   },
+   validationEmail() {
+      field = document.getElementById("js-email-field");
+      if (field == null) {
+         field = "";
+      }
       usuario = field.value.substring(0, field.value.indexOf("@"));
       dominio = field.value.substring(
          field.value.indexOf("@") + 1,
@@ -17,11 +28,13 @@ const form = {
          dominio.indexOf(".") >= 1 &&
          dominio.lastIndexOf(".") < dominio.length - 1
       ) {
-         document.getElementById("js-error-message").style.display = "none";
-         document.getElementById("js-error-img").style.display = "none";
+         // document.getElementById("js-error-message").style.display = "none";
+         // document.getElementById("js-error-img").style.display = "none";
+         return true;
       } else {
-         document.getElementById("js-error-message").style.display = "block";
-         document.getElementById("js-error-img").style.display = "block";
+         // document.getElementById("js-error-message").style.display = "block";
+         // document.getElementById("js-error-img").style.display = "block";
+         return false;
       }
    },
 
@@ -31,5 +44,44 @@ const form = {
       var padrao = /[^a-zà-ú]/gi;
       var valida_nome = name.match(padrao);
       var valida_sobrenome = lastName.match(padrao);
+      if (valida_nome || !nome) {
+         return false;
+      } else {
+         return true;
+      }
+   },
+
+   validaçãoSenha(p) {
+      var retorno = false;
+      var letrasMaiusculas = /[A-Z]/;
+      var letrasMinusculas = /[a-z]/;
+      var numeros = /[0-9]/;
+      var caracteresEspeciais = /[!|@|#|$|%|^|&|*|(|)|-|_]/;
+      if (p.length < 3) {
+         return "senha fraca";
+      }
+      if (p.length < 16) {
+         return "senha grande demais";
+      }
+      var auxMaiuscula = 0;
+      var auxMinuscula = 0;
+      var auxNumero = 0;
+      var auxEspecial = 0;
+      for (var i = 0; i < p.length; i++) {
+         if (letrasMaiusculas.test(p[i])) auxMaiuscula++;
+         else if (letrasMinusculas.test(p[i])) auxMinuscula++;
+         else if (numeros.test(p[i])) auxNumero++;
+         else if (caracteresEspeciais.test(p[i])) auxEspecial++;
+      }
+      if (auxMaiuscula > 0) {
+         if (auxMinuscula > 0) {
+            if (auxNumero > 0) {
+               if (auxEspecial) {
+                  retorno = true;
+               }
+            }
+         }
+      }
+      return retorno;
    },
 };
