852a39974a70e2f0a797238c10a6e02301b5ba00
Pluto <thyagohyung@gmail.com>
2021-07-23 18:17:50 -0300

    Projeto de border-radius completo

--- iniciante/Border-radius Previewer/script.js
+++ iniciante/Border-radius Previewer/script.js
@@ -2,13 +2,15 @@ let left = document.getElementById("js-left");
 let right = document.getElementById("js-right");
 let tops = document.getElementById("js-top");
 let bottom = document.getElementById("js-bottom");
-document.getElementById("js-border-radius").innerHTML = `Border-Radius: ${
-   tops.value
-}% ${(tops.value - 100) * -1}% ${bottom.value}% ${
-   (bottom.value - 100) * -1
-}% / ${left.value}% ${right.value}% ${(right.value - 100) * -1}% ${
-   (left.value - 100) * -1
-}%`;
+let topValue = (tops.value - 100) * -1;
+let leftValue = (left.value - 100) * -1;
+let rightValue = (right.value - 100) * -1;
+let bottomValue = (bottom.value - 100) * -1;
+document.getElementById("js-border-radius").innerHTML = `${tops.value}% ${
+   (tops.value - 100) * -1
+}% ${bottom.value}% ${(bottom.value - 100) * -1}% / ${left.value}% ${
+   right.value
+}% ${(right.value - 100) * -1}% ${(left.value - 100) * -1}%`;
 var Draggable = function (elemento) {
    elementoAtual = elemento;
    this.refMouseUp = function (event) {
@@ -23,16 +25,12 @@ var Draggable = function (elemento) {
       elemento.classList.add("dragging");
 
       window.addEventListener("mousemove", function () {
-         let topValue = (tops.value - 100) * -1;
-         let leftValue = (left.value - 100) * -1;
-         let rightValue = (right.value - 100) * -1;
-         let bottomValue = (bottom.value - 100) * -1;
          document.getElementById(
             "js-shape-generator"
          ).style.borderRadius = `${tops.value}% ${topValue}% ${bottomValue}% ${bottom.value}% / ${left.value}% ${right.value}% ${rightValue}% ${leftValue}%`;
          document.getElementById(
             "js-border-radius"
-         ).innerHTML = `Border-Radius: ${tops.value}% ${topValue}% ${bottomValue}% ${bottom.value}% / ${left.value}% ${right.value}% ${rightValue}% ${leftValue}%`;
+         ).innerHTML = `${tops.value}% ${topValue}% ${bottomValue}% ${bottom.value}% / ${left.value}% ${right.value}% ${rightValue}% ${leftValue}%`;
       });
 
       window.addEventListener("mouseup", function () {
@@ -46,3 +44,8 @@ var draggables = document.querySelectorAll(".draggable");
 [].forEach.call(draggables, function (draggable, indice) {
    new Draggable(draggable);
 });
+
+function copy() {
+   let textArea = `border-radius: ${tops.value}% ${topValue}% ${bottomValue}% ${bottom.value}% / ${left.value}% ${right.value}% ${rightValue}% ${leftValue}%;`;
+   navigator.clipboard.writeText(textArea);
+}
