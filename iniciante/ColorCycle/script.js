3986767b77560b56b0d46ebbde6c03f88fac6240
Pluto <thyagohyung@gmail.com>
2021-08-18 23:50:20 -0300

    Terminando desafio js

--- iniciante/ColorCycle/script.js
+++ iniciante/ColorCycle/script.js
@@ -1,8 +1,45 @@
-function colorCycle() {
-   document.querySelector(".btn").classList.toggle("btn-start");
-   document.querySelector(".btn").classList.toggle("btn-stop");
-   let test = document.querySelector(".btn").classList;
-   if (test != -1) {
+let red = document.getElementById("js-red");
+let green = document.getElementById("js-green");
+let blue = document.getElementById("js-blue");
+
+document.getElementById("js-green").max;
+
+function enforceMinMax(el) {
+   if (el.value != "") {
+      if (parseInt(el.value) < parseInt(el.min)) {
+         el.value = el.min;
+      }
+      if (parseInt(el.value) > parseInt(el.max)) {
+         el.value = el.max;
+      }
    }
-   console.log(test.indexOf("btn-start"));
+}
+
+function colorCycle() {
+   if (document.querySelector(".btn").innerHTML == "Start") {
+      document.querySelector(".btn").innerHTML = "Stop";
+      document.querySelector(".btn").classList.add("btn-stop");
+      document.querySelector(".btn").classList.remove("btn-start");
+   } else if (document.querySelector(".btn").innerHTML == "Stop") {
+      document.querySelector(".btn").classList.remove("btn-stop");
+      document.querySelector(".btn").classList.add("btn-start");
+      document.querySelector(".btn").innerHTML = "Start";
+   }
+
+   if (red.value && green.value && blue.value > 0) {
+      alert("test");
+      if (red.value < 10) {
+         red.value = `0${red.value}`;
+      }
+      if (green.value < 10) {
+         green.value = `0${green.value}`;
+      }
+      if (blue.value < 10) {
+         blue.value = `0${blue.value}`;
+      }
+
+      document.querySelector(
+         "body"
+      ).style.background = `#${red.value}${green.value}${blue.value}`;
+   }
 }
