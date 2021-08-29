9c518f98f7715db1f9047e0e663c3275ee62edad
Pluto <thyagohyung@gmail.com>
2021-08-28 23:33:39 -0300

    Desafio Js Countdown Timer Finalizado

---
+++ iniciante/CountdownTimer/script.js
@@ -0,0 +1,90 @@
+let timeInterval;
+let timeIntervalCheck = false;
+
+const countDownTimer = {
+   countDown: function () {
+      if (countDownTimer.verify()) {
+         let checkButton = document.getElementById("js-button-start").innerHTML;
+         if (checkButton == "Start") {
+            document.getElementById("js-button-start").innerHTML = "Stop";
+            countDownTimer.startCountDown();
+         } else if (checkButton == "Stop") {
+            document.getElementById("js-button-start").innerHTML = "Start";
+            countDownTimer.stopCountDown();
+         }
+      } else {
+         alert("Digite o nome para o Evento");
+      }
+   },
+   verify: () => {
+      let name = document.getElementById("js-name").value;
+      let date = document.getElementById("js-date").value;
+
+      if (name != "" && date != "") {
+         return true;
+      } else {
+         return false;
+      }
+   },
+   startCountDown: () => {
+      let name = document.getElementById("js-name").value;
+      document.getElementById("js-eventName").innerHTML = name;
+
+      let hoursMinInput = document.getElementById("js-time").value;
+      let dateInput = document.getElementById("js-date").value.split("-");
+
+      let FutureDate;
+      if (hoursMinInput != "") {
+         hoursMinInput.split(":");
+         FutureDate = new Date(
+            dateInput[0],
+            dateInput[1],
+            dateInput[2],
+            hoursMinInput[1],
+            hoursMinInput[0]
+         );
+      } else {
+         FutureDate = new Date(
+            ` ${dateInput[1]} ${Number(dateInput[2])} ${dateInput[0]}`
+         );
+      }
+
+      let currentTime = new Date();
+      let totalSeconds = (FutureDate - currentTime) / 1000;
+      if (totalSeconds < 0) {
+         countDownTimer.stopCountDown();
+         return;
+      }
+
+      let months = Math.floor(totalSeconds / 3600 / 24 / 30);
+      let days = Math.floor(totalSeconds / 3600 / 24);
+      let hours = Math.floor(totalSeconds / 3600) % 24;
+      let minutes = Math.floor(totalSeconds / 60) % 60;
+      let seconds = Math.floor(totalSeconds % 60);
+
+      document.getElementById(
+         "js-time-counter"
+      ).innerHTML = `${months}m ${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+      if (timeIntervalCheck == false) {
+         timeInterval = setInterval(() => {
+            countDownTimer.startCountDown();
+         }, 1000);
+         document.querySelectorAll("input").forEach((input) => {
+            input.readOnly = true;
+         });
+         timeIntervalCheck = true;
+      }
+   },
+   stopCountDown: () => {
+      clearInterval(timeInterval);
+      timeIntervalCheck = false;
+      document.querySelectorAll("input").forEach((input) => {
+         input.readOnly = false;
+      });
+   },
+};
+
+document.getElementById("js-button-start").addEventListener("click", () => {
+   countDownTimer.countDown();
+});
