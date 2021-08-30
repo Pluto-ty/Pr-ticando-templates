209c5e9d6c572b787d0eb7b8692d63303ca8e44c
Pluto <thyagohyung@gmail.com>
2021-08-29 23:19:41 -0300

    Adicionando novas coisas ao CountDown Timer

--- iniciante/CountdownTimer/script.js
+++ iniciante/CountdownTimer/script.js
@@ -4,6 +4,14 @@ let timeIntervalCheck = false;
 const countDownTimer = {
    countDown: function () {
       if (countDownTimer.verify()) {
+         let FutureDate = countDownTimer.futureDateCalc();
+         let currentTime = new Date();
+         if (FutureDate < currentTime) {
+            console.log(FutureDate);
+            console.log("Errado");
+            return;
+         }
+
          let checkButton = document.getElementById("js-button-start").innerHTML;
          if (checkButton == "Start") {
             document.getElementById("js-button-start").innerHTML = "Stop";
@@ -27,31 +35,12 @@ const countDownTimer = {
       }
    },
    startCountDown: () => {
-      let name = document.getElementById("js-name").value;
-      document.getElementById("js-eventName").innerHTML = name;
-
-      let hoursMinInput = document.getElementById("js-time").value;
-      let dateInput = document.getElementById("js-date").value.split("-");
-
-      let FutureDate;
-      if (hoursMinInput != "") {
-         hoursMinInput.split(":");
-         FutureDate = new Date(
-            dateInput[0],
-            dateInput[1],
-            dateInput[2],
-            hoursMinInput[1],
-            hoursMinInput[0]
-         );
-      } else {
-         FutureDate = new Date(
-            ` ${dateInput[1]} ${Number(dateInput[2])} ${dateInput[0]}`
-         );
-      }
+      let FutureDate = countDownTimer.futureDateCalc();
 
       let currentTime = new Date();
       let totalSeconds = (FutureDate - currentTime) / 1000;
       if (totalSeconds < 0) {
+         console.log("Cabou");
          countDownTimer.stopCountDown();
          return;
       }
@@ -83,8 +72,30 @@ const countDownTimer = {
          input.readOnly = false;
       });
    },
+   futureDateCalc: () => {
+      let name = document.getElementById("js-name").value;
+      let hoursMinInput = document.getElementById("js-time").value;
+      let dateInput = document.getElementById("js-date").value.split("-");
+      document.getElementById("js-eventName").innerHTML = name;
+
+      let FutureDate;
+      if (hoursMinInput != "") {
+         hoursMinInput.split(":");
+         console.log(hoursMinInput);
+         FutureDate = new Date(
+            `${dateInput[0]}-${dateInput[1]}-${dateInput[2]}T${hoursMinInput[1]}:${hoursMinInput[0]}:00`
+         );
+      } else {
+         FutureDate = new Date(
+            ` ${dateInput[1]} ${Number(dateInput[2])} ${dateInput[0]}`
+         );
+      }
+      return FutureDate;
+   },
 };
 
 document.getElementById("js-button-start").addEventListener("click", () => {
    countDownTimer.countDown();
 });
+
+// document.getElementById("js-save").addEventListener("")
